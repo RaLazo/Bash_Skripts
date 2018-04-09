ccfc6513d2aff5f48adcf9277b21a06452b45b1e
Lazen <rafaellazenhofer@gmail.com>
2018-04-09 08:58:14 +0200

    Edit sting snipping

--- OpenSUSE/ceph.sh
+++ OpenSUSE/ceph.sh
@@ -21,7 +21,10 @@ host='node1'
 
 #Beginn Code:
 #OSD & HEALTH - Abfrage:
-stat=$(ceph -s | grep osdmap);osd=${stat:17:1};up=${stat:25:1};in=${stat:31:1};
+stat=$(ceph -s | grep osdmap);
+osd=${stat:14:1};
+up=${stat:26:1};
+in=${stat:32:1};
 #�bertragung:
 #OSD & HEALTH - status:
 curl -XPOST "http://192.168.1.11:8086/write?db=osd" -d "cluster,osd=${osd},up=${up} in=${in}"
